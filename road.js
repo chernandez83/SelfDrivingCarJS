579045e7c63d8c57c41d2417d8497bfbc5f2221a
chernandez83 <104525460+chernandez83@users.noreply.github.com>
2022-07-01 18:54:30 -0500

    Add sensors

--- road.js
+++ road.js
@@ -10,13 +10,27 @@ class Road {
         const infinity = 1000000;
         this.top = -infinity;
         this.bottom = infinity;
+
+        const topLeft = {x: this.left, y: this.top};
+        const topRight = {x: this.right, y: this.top};
+        const bottomLeft = {x: this.left, y: this.bottom};
+        const bottomRight = {x: this.right, y: this.bottom};
+        this.borders = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight]
+        ];
     };
+
+    getLaneCenter(laneIndex=0) {
+        const laneWidth = this.width / this.lanes;
+        return this.left + laneWidth/2 + Math.min(laneIndex, this.lanes - 1) * laneWidth;
+    }
 
     draw(ctx) {
         ctx.lineWidth = 5;
         ctx.strokeStyle = 'white';
 
-        for(let i = 0; i <= this.lanes; i++) {
+        for(let i = 1; i <= this.lanes - 1; i++) {
             const x = lerp(
                 this.left,
                 this.right,
@@ -34,6 +48,14 @@ class Road {
             ctx.lineTo(x, this.bottom);
             ctx.stroke();
         }
+
+        ctx.setLineDash([]);
+        this.borders.forEach((border) => {
+            ctx.beginPath();
+            ctx.moveTo(border[0].x, border[0].y);
+            ctx.lineTo(border[1].x, border[1].y);
+            ctx.stroke();
+        });
 
         /*ctx.beginPath();
         ctx.moveTo(this.left, this.top);
